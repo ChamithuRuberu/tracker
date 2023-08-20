294ccfdbd5fb45af1e4c74d56713c360f377b757
tcdruberu <94827905+tcdruberu@users.noreply.github.com>
2023-08-20 15:39:32 +0530

    done

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'tracker'`, () => {
+  it(`should have as title 'my-tracker'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('tracker');
+    expect(app.title).toEqual('my-tracker');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('tracker app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('my-tracker app is running!');
   });
 });
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,27 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ShareModule } from './modules/share/share.module';
+import { NotFoundComponent } from './core/not-found/not-found.component';
+import { MainFooterComponent } from './core/main-footer/main-footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {AngularFireModule} from "@angular/fire/compat"
+import {AngularFireAuthModule} from "@angular/fire/compat/auth"
+import { environment } from 'src/environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotFoundComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ShareModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig), // Initialize Firebase with your config
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
